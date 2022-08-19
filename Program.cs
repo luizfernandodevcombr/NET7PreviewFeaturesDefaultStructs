9731faabfb5236b794a25fb4d1072a6f8dd773da
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2022-08-19 20:21:47 -0300

    included the project files

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Se você não inicializar no C# o atributo no construtor, terá o erro de compilação.
+//Mas, no caso do C# 11, este código irá compilar sem problemas.
+public struct MinhaStruct
+{
+    public int Numero { get; set; }
+
+    public MinhaStruct()
+    {
+    }
+}
